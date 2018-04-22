b4322ce0fff303e09aca09abbfd2bf8a45dc22ce
chencong-plan <1042738887@qq.com>
2018-04-22 16:13:08 +0800

    update webpack.config.json

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,8 +2,9 @@
  * @Author: chencong 
  * @Date: 2018-04-22 14:56:07 
  * @Last Modified by: chencong
- * @Last Modified time: 2018-04-22 15:36:28
+ * @Last Modified time: 2018-04-22 16:01:23
  */
 
 require('../module.js')
+require('./index.css');
 console.log('hello index')
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@
  * @Author: chencong 
  * @Date: 2018-04-22 14:52:17 
  * @Last Modified by: chencong
- * @Last Modified time: 2018-04-22 15:39:05
+ * @Last Modified time: 2018-04-22 16:09:42
  */
 var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var config = {
   entry: {
-    common: ['./src/page/common/index.js'],
+    common: ["./src/page/common/index.js"],
     index: ["./src/page/index/index.js"],
     login: ["./src/page/login/index.js"]
   },
@@ -18,11 +19,20 @@ var config = {
   externals: {
     jquery: "window.jQuery"
   },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      }
+    ]
+  },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
-      name: "commons",
+      name: "common",
       filename: "js/base.js"
-    })
+    }),
+    new ExtractTextPlugin("css/[name].css")
   ]
 };
 
