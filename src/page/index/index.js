ed43cfa673f0f0d8a5203d6ca3c8c1a3ecbaae43
chencong-plan <1042738887@qq.com>
2018-04-22 20:31:24 +0800

    完成mm.js工具类

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,7 +2,7 @@
  * @Author: chencong 
  * @Date: 2018-04-22 14:56:07 
  * @Last Modified by: chencong
- * @Last Modified time: 2018-04-22 19:55:54
+ * @Last Modified time: 2018-04-22 20:18:25
  */
 
 var _mm = require("util/mm.js");
@@ -23,4 +23,13 @@ var _mm = require("util/mm.js");
 /**
  * js工具类封装测试
  */
-console.log(_mm.getUrlParam('test'));
+console.log("渲染获取参数测试:test:" + _mm.getUrlParam("test"));
+
+/**
+ * 测试hogan模板
+ */
+var html = "<div>{{data}}</div>";
+var data = {
+  data: "123"
+};
+console.log("渲染html代码测试：" + _mm.renderHtml(html, data));
--- src/util/mm.js
+++ src/util/mm.js
@@ -2,9 +2,14 @@
  * @Author: chencong 
  * @Date: 2018-04-22 19:03:44 
  * @Last Modified by: chencong
- * @Last Modified time: 2018-04-22 19:50:51
+ * @Last Modified time: 2018-04-22 20:30:37
  * 通用工具类
  */
+
+/**
+ * 引入打包插件
+ */
+var Hogan = require("hogan.js");
 
 /**
  * 配置文件
@@ -65,12 +70,61 @@ var __mm = {
     var result = window.location.search.substr(1).match(reg);
     return result ? decodeURIComponent(result[2]) : null;
   },
+
+  /**
+   * 渲染HTML模板,
+   * hogan先编译后渲染
+   */
+  renderHtml: function(htmlTemplate, data) {
+    var template = Hogan.compile(htmlTemplate),
+      result = template.render(data);
+    return result;
+  },
+  /**
+   * 成功提示
+   */
+  successTips: function(msg) {
+    alert(msg || "操作成功");
+  },
+  /**
+   * 错误提示
+   */
+  errorTips: function(msg) {
+    alert(msg || "哪里不对了~");
+  },
+  /**
+   * 字段的验证，支持非空，手机，邮箱的判断
+   * value:将要验证的字符串
+   * type：验证的类型，是否为空，是否为手机号，是否为邮箱
+   */
+  validate: function(value, type) {
+    var value = $.trim(value);
+    //非空验证
+    if ("require" === type) {
+      return !!value;
+    }
+    //手机号验证
+    if ("phone" === type) {
+      return /^1\d{10}$/.test(value);
+    }
+    //邮箱验证
+    if ("email" === type) {
+      return /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/.test(value);
+    }
+  },
+
   /**
    * 统一登录处理
    */
   doLogin: function() {
     window.location.href =
       "./login.html?redirect=" + encodeURIComponent(window.location.href);
+  },
+  /**
+   * 跳转回首页
+   */
+  goHome: function() {
+    window.location.href = "./index.html";
   }
 };
 
