5150b3d0a48c593bd8bc925b089046bc6c35a66c
chencong-plan <1042738887@qq.com>
2018-04-26 10:43:10 +0800

    添加商品列表页

--- src/page/list/index.js
+++ src/page/list/index.js
@@ -2,13 +2,14 @@
  * @Author: chencong
  * @Date: 2018-04-25 21:54:34
  * @Last Modified by: chencong
- * @Last Modified time: 2018-04-25 22:19:52
+ * @Last Modified time: 2018-04-26 10:42:03
  */
 require("./index.css");
 require("page/common/nav/index.js");
 require("page/common/header/index.js");
 var _mm = require("util/mm.js");
 var _product = require("service/product-service.js");
+var Pagination = require("util/pagination/index.js");
 var templateIndex = require("./index.string");
 
 var page = {
@@ -16,7 +17,7 @@ var page = {
         listParam: {
             keyword: _mm.getUrlParam("keyword") || "",
             categoryId: _mm.getUrlParam("categoryId") || "",
-            orderBy: _mm.getUrlParam("orderBy") || "desc",
+            orderBy: _mm.getUrlParam("orderBy") || "default",
             pageNum: _mm.getUrlParam("pageNum") || 1,
             pageSize: _mm.getUrlParam("pageSize") || 20
         }
@@ -28,24 +29,74 @@ var page = {
     onLoad: function() {
         this.loadList();
     },
-    // 绑定数据
-    bindEvent: function() {},
-    //加载list数据
-    loadList: function() {
-        var listParam = this.data.listParam;
-        var listHtml = "";
+    bindEvent: function() {
         var _this = this;
+        // 排序的点击事件
+        $(".sort-item").click(function() {
+            var $this = $(this);
+            _this.data.listParam.pageNum = 1;
+            // 点击默认排序
+            if ($this.data("type") === "default") {
+                // 已经是active样式
+                if ($this.hasClass("active")) {
+                    return;
+                }
+                // 其他
+                else {
+                    $this
+                        .addClass("active")
+                        .siblings(".sort-item")
+                        .removeClass("active asc desc");
+                    _this.data.listParam.orderBy = "default";
+                }
+            }
+            // 点击价格排序
+            else if ($this.data("type") === "price") {
+                // active class 的处理
+                $this
+                    .addClass("active")
+                    .siblings(".sort-item")
+                    .removeClass("active asc desc");
+                // 升序、降序的处理
+                if (!$this.hasClass("asc")) {
+                    $this.addClass("asc").removeClass("desc");
+                    _this.data.listParam.orderBy = "price_asc";
+                } else {
+                    $this.addClass("desc").removeClass("asc");
+                    _this.data.listParam.orderBy = "price_desc";
+                }
+            }
+            // 重新加载列表
+            _this.loadList();
+        });
+    },
+    // 加载list数据
+    loadList: function() {
+        var _this = this,
+            listHtml = "",
+            listParam = this.data.listParam,
+            $pListCon = $(".p-list-con");
+        $pListCon.html('<div class="loading"></div>');
+        // 删除参数中不必要的字段
+        listParam.categoryId
+            ? (delete listParam.keyword)
+            : (delete listParam.categoryId);
+        // 请求接口
         _product.getProductList(
             listParam,
             function(res) {
-                // 请求成功，开始渲染
-                listHtml = _mm.renderHtml(templateIndex,{
-                    list : res.list
+                listHtml = _mm.renderHtml(templateIndex, {
+                    list: res.list
                 });
-                // 填充渲染之后的html
-                $('.p-list.con').html(listHtml);
-                // 加载分页信息
-                _this.loadPagination(res.pageNum,res.pages);
+                $pListCon.html(listHtml);
+                _this.loadPagination({
+                    hasPreviousPage: res.hasPreviousPage,
+                    prePage: res.prePage,
+                    hasNextPage: res.hasNextPage,
+                    nextPage: res.nextPage,
+                    pageNum: res.pageNum,
+                    pages: res.pages
+                });
             },
             function(errMsg) {
                 _mm.errorTips(errMsg);
@@ -53,11 +104,20 @@ var page = {
         );
     },
     // 加载分页信息
-    loadPagination : function(pageNum,pages){
-
+    loadPagination: function(pageInfo) {
+        var _this = this;
+        this.pagination ? "" : (this.pagination = new Pagination());
+        this.pagination.render(
+            $.extend({}, pageInfo, {
+                container: $(".pagination"),
+                onSelectPage: function(pageNum) {
+                    _this.data.listParam.pageNum = pageNum;
+                    _this.loadList();
+                }
+            })
+        );
     }
 };
-
 $(function() {
     page.init();
 });
